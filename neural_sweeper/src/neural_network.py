b4e7c6a3e321991627066a87f56894d6a3f64fa3
Dalton Prokosch <103225424+garthable@users.noreply.github.com>
2025-04-30 13:49:47 -0600

    creted basic neural network

---
+++ neural_sweeper/src/neural_network.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+from hyperparameters import *
+
+class MinesweeperDQN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # Identify Features of Board
+        self.convolution = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(),
+        )
+
+        # Neural Deep Q 
+        self.fully_connected = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(64 * NUM_ACTIONS, 512),
+            nn.ReLU(),
+            nn.Linear(512, NUM_ACTIONS)
+        )
+
+    def forward(self, x):
+        x = self.convolution(x)
+        return self.fully_connected(x)
